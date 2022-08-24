49d33f7b45fee6d19a73e8adebfa4e6379d8bb2d
chenlong <chenlong1@jkinvest.cn>
2022-08-24 16:16:52 +0800

    build: update

--- packages/cat-ui-vite/vite.config.ts
+++ packages/cat-ui-vite/vite.config.ts
@@ -1,14 +1,15 @@
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueJsx from "@vitejs/plugin-vue-jsx";
 import Unocss from "./config/unocss";
 
 // https://vitejs.dev/config/
 
 const rollupOptions = {
-external: ["vue", "vue-router"],
+  external: ["vue", "vue-router"],
   output: {
+    assetFileNames: "[name].[ext]",
+    exports: "named",
     globals: {
       vue: "Vue",
     },
@@ -16,35 +17,35 @@ external: ["vue", "vue-router"],
 };
 
 export default defineConfig({
-  resolve: { alias: { "vue": "vue/dist/vue.esm-bundler.js" } },
+  resolve: { alias: { vue: "vue/dist/vue.esm-bundler.js" } },
 
   plugins: [
-      vue(),
+    vue(),
     // 添加JSX插件
     vueJsx({
       // options are passed on to @vue/babel-plugin-jsx
     }),
     // 添加UnoCSS插件
-    Unocss()
+    Unocss(),
   ],
   test: {
     // enable jest-like global test APIs
     globals: true,
     // simulate DOM with happy-dom
     // (requires installing happy-dom as a peer dependency)
-    environment: 'happy-dom',
+    environment: "happy-dom",
     // 支持tsx组件，很关键
     transformMode: {
-      web: [/.[tj]sx$/]
-    }
+      web: [/.[tj]sx$/],
+    },
   },
 
   // 添加库模式配置
   build: {
     rollupOptions,
-    minify: 'terser', // boolean | 'terser' | 'esbuild'
+    minify: "terser", // boolean | 'terser' | 'esbuild'
     sourcemap: true, // 输出单独 source文件
-    brotliSize: true,  // 生成压缩大小报告
+    brotliSize: true, // 生成压缩大小报告
     cssCodeSplit: true,
     lib: {
       entry: "./src/entry.ts",
@@ -53,5 +54,4 @@ export default defineConfig({
       formats: ["esm", "umd", "iife"], // 导出模块类型
     },
   },
-
 });
